b5f4f232e57dd9aec52da5ce53cf9bb88c83dac3
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-06-22 22:48:30 +0900

    feat: 존재하는 매장인지에 대한 테스트 코드 추가

--- src/modules/bookmark/bookmark.service.spec.ts
+++ src/modules/bookmark/bookmark.service.spec.ts
@@ -2,17 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BookmarkService } from './bookmark.service';
 
 describe('BookmarkService', () => {
-  let service: BookmarkService;
+    let bookmarkService: BookmarkService;
+    let storeRepositry: any;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BookmarkService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [BookmarkService],
+        }).compile();
 
-    service = module.get<BookmarkService>(BookmarkService);
-  });
+        bookmarkService = module.get<BookmarkService>(BookmarkService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(bookmarkService).toBeDefined();
+    });
+
+    describe('addFavoriteStore', () => {
+        const storeId = 1;
+        it('존재하는 매장인지 확인', async () => {
+            jest.spyOn(storeRepositry, 'findOneStore').mockResolvedValueOnce(true);
+            const result = await bookmarkService.findOneStore(storeId);
+            expect(result).not.toBeNull();
+        });
+    });
 });
--- src/modules/delivery/delivery.controller.ts
+++ src/modules/delivery/delivery.controller.ts
@@ -5,30 +5,30 @@ import { UpdateDeliveryDto } from './dto/update-delivery.dto';
 
 @Controller('delivery')
 export class DeliveryController {
-  constructor(private readonly deliveryService: DeliveryService) {}
+    constructor(private readonly deliveryService: DeliveryService) {}
 
-  @Post()
-  create(@Body() createDeliveryDto: CreateDeliveryDto) {
-    return this.deliveryService.create(createDeliveryDto);
-  }
+    @Post()
+    create(@Body() createDeliveryDto: CreateDeliveryDto) {
+        return this.deliveryService.create(createDeliveryDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.deliveryService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.deliveryService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.deliveryService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.deliveryService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDeliveryDto: UpdateDeliveryDto) {
-    return this.deliveryService.update(+id, updateDeliveryDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateDeliveryDto: UpdateDeliveryDto) {
+        return this.deliveryService.update(+id, updateDeliveryDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.deliveryService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.deliveryService.remove(+id);
+    }
 }
