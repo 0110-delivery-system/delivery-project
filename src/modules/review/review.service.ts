002f45cfa00f8e674c637719c47aa1877f0658bf
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-06-25 20:32:38 +0900

    feat: 리뷰 테스트코드 작성 완료

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
-import { CreatereviewDto } from './dto/create-review.dto';
+import { IReviewRepository } from './review.IRepository';
+import { Inject, Injectable, BadRequestException } from '@nestjs/common';
 import { UpdatereviewDto } from './dto/update-review.dto';
-
+import { FakeAuthService, FakeOrderService } from './review.service.spec';
 @Injectable()
-export class reviewService {
-    create(createreviewDto: CreatereviewDto) {
-        return 'This action adds a new review';
+export class ReviewService {
+    constructor(@Inject(IReviewRepository) private reviewRepository: IReviewRepository) {}
+    authService = new FakeAuthService();
+    orderService = new FakeOrderService();
+
+    async createReview(orderId: number, userId: number, review: any) {
+        const createResult = this.reviewRepository.saveReview(orderId, userId, review);
+        return createResult;
     }
 
-    findAll() {
-        return `This action returns all review`;
+    async validateReview(userId: number, orderId: number, review: any) {
+        const user = await this.authService.getUser(userId);
+        const order = await this.orderService.getOrder(orderId);
+
+        if (user === null) {
+            throw new BadRequestException('존재하지 않는 유저입니다.');
+        }
+        if (order === null) {
+            throw new BadRequestException('존재하지 않는 주문입니다.');
+        }
+        if (order.userId !== userId) {
+            throw new BadRequestException('해당 주문을 작성한 유저가 아닙니다.');
+        }
+        if (order.review) {
+            throw new BadRequestException('이미 리뷰를 작성한 주문입니다.');
+        }
+        if (!review.title) {
+            throw new BadRequestException('리뷰의 제목을 입력해주세요.');
+        }
+        if (!review.content) {
+            throw new BadRequestException('리뷰의 내용을 입력해주세요.');
+        }
+        if (order.status !== '배달완료') {
+            throw new BadRequestException('배달완료된 주문만 리뷰를 작성할 수 있습니다.');
+        }
+
+        const addHours = (date, hours) => {
+            const newDate = new Date(date.getTime());
+            newDate.setHours(date.getHours() + hours);
+            return newDate;
+        };
+
+        const currentTime = new Date();
+        const oneHourAfterDelivery = addHours(order.time, 1);
+        const twentyFourHoursAfterDelivery = addHours(order.time, 24);
+        if (oneHourAfterDelivery > currentTime) {
+            throw new BadRequestException('배달완료 후 1시간 이후에 리뷰를 작성할 수 있습니다.');
+        }
+
+        if (twentyFourHoursAfterDelivery < currentTime) {
+            throw new BadRequestException('리뷰 작성 기간이 지났습니다.');
+        }
+
+        return true;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} review`;
-    }
-
-    update(id: number, updatereviewDto: UpdatereviewDto) {
-        return `This action updates a #${id} review`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} review`;
+    async getReview(userId: number, orderId: number) {
+        const result = await this.reviewRepository.findOneReview(orderId, userId);
+        return result ?? null;
     }
 }
