18f84f5efada18e2ef1c39bcb5611469f264c6db
taesik <taesikyoon@10k1m.com>
2023-06-27 00:25:53 +0900

    Feat : set module

--- src/modules/owner/owner.module.ts
+++ src/modules/owner/owner.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OwnerService } from './owner.service';
 import { OwnerController } from './owner.controller';
+import { MockOwnerRepository } from './owner.service.spec';
+import { IOwnerRepository } from './owner.IRepository';
 
 @Module({
-  controllers: [OwnerController],
-  providers: [OwnerService]
+    controllers: [OwnerController],
+    providers: [
+        OwnerService,
+        {
+            provide: IOwnerRepository,
+            useClass: MockOwnerRepository,
+        },
+    ],
 })
 export class OwnerModule {}
