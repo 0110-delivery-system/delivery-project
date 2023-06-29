c908a4c286d3986acd70946ab966259a81c8686a
taesik <taesikyoon@10k1m.com>
2023-06-29 22:18:11 +0900

    Feat : 생성,조회 테스트 코드 작성

--- src/modules/store/store.service.spec.ts
+++ src/modules/store/store.service.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 @Injectable()
 class MockRepository {
-    async findOneByOwnerId(ownerId: number) {
+    async findOneById(ownerId: any) {
         const storeInfo = {
             id: 1,
             ownerId: 1,
@@ -48,7 +48,7 @@ class StoreService {
     constructor(private readonly mockRepository: MockRepository) {}
 
     async createStore(storeName: string, address: string, ownerId: number) {
-        const existOwnerStore = await this.mockRepository.findOneByOwnerId(ownerId);
+        const existOwnerStore = await this.getStore(ownerId);
 
         if (existOwnerStore) throw new BadRequestException('한개의 가게만 등록이 가능합니다.');
 
@@ -60,6 +60,10 @@ class StoreService {
         const storeList = await this.mockRepository.getMany();
 
         return storeList;
+    }
+
+    async getStore(ownerId: number) {
+        return await this.mockRepository.findOneById(ownerId);
     }
 }
 describe('StoreService', () => {
@@ -118,31 +122,28 @@ describe('StoreService', () => {
                 },
             ];
 
-            // 매장 카테고리 또는 지역별? 필요한거같은ㄷ ㅔ졸리니 패스
             expect(await service.getManyStore()).toEqual(storeList);
         });
     });
 
     describe('하나의 매장 정보를 제공한다.', () => {
-        it('WLWL', () => {
-            expect('');
+        it('사장님의 매장이 존재한다면 매장정보를 제공한다. - 성공', async () => {
+            const ownerId = 1;
+
+            const storeInfo = {
+                id: 1,
+                ownerId: 1,
+                storeName: '다른 수프집',
+                address: '주소',
+            };
+
+            expect(await service.getStore(ownerId)).toEqual(storeInfo);
+        });
+
+        it('사장님의 매장이 존재하지않는다면 매장정보를 제공하지 못한다. - 실패', async () => {
+            const ownerId = 3;
+
+            expect(await service.getStore(ownerId)).toBeNull();
         });
     });
 });
-// - 매장 조회 기능 ( getManyStore )
-//     - 준비물 : DB ( 매장(오너컬럼) )
-//     - ***사장 하나 =  매장 하나***
-// - 음식점 정보 등록 Unit ( createStore() )
-//     - 준비물: DB (store, owner)
-//     - 성공
-//         - DB에 음식점 정보를 등록한다
-//     - 실패
-//         - 존재하지 않는 Owner일 경우
-//         - Owner나 store 이름 등 필요한 값이 존재하지 않을 경우
-// - 음식점 정보 검증 Unit (validateStore() )
-//     - 준비물: DB (store)
-//     - 성공
-//         - 음식점 정보 검증에 성공했을 경우
-//     - 실패
-//         - 이미 존재하는 음식점일 경우
-//         - Owner나 store 이름 등 필요한 값이 존재하지 않을 경우
