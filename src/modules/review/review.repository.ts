1d0e02e479b92a5115e4c3ba52608ac11a168a3a
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-07-08 00:34:50 +0900

    feat: 리뷰 레파지토리 코드 작성완료

--- src/modules/review/review.repository.ts
+++ src/modules/review/review.repository.ts
@@ -1,12 +1,29 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { IReviewRepository } from './review.IRepository';
 import { Injectable } from '@nestjs/common';
+import { Review } from './entities/review.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ReviewRepository implements IReviewRepository {
-    saveReview(orderId: number, userId: number, review: any) {
-        return true;
+    constructor(
+        @InjectRepository(Review)
+        private reviewModel: Repository<Review>
+    ) {}
+
+    async saveReview(orderId: number, userId: number, review: any): Promise<Review> {
+        const newReview = this.reviewModel.create();
+        newReview.OrderId = orderId;
+        newReview.UserId = userId;
+        newReview.title = review.title;
+        newReview.content = review.content;
+
+        const savedReview = await this.reviewModel.save(newReview);
+        return savedReview;
     }
-    findOneReview(userId: number, orderId: number) {
-        return true;
+
+    async findOneReview(userId: number, orderId: number): Promise<Review | null> {
+        const review = await this.reviewModel.findOne({ where: { UserId: userId, OrderId: orderId } });
+        return review || null;
     }
 }
